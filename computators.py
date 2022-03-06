01d454f664d194e16a06dba18aded7df38804702
DennisDv24 <jokarboxing@gmail.com>
2022-03-06 14:56:14 +0100

    simple newton optimizator and plotter

---
+++ computators.py
@@ -0,0 +1,43 @@
+import numdifftools as nd
+import numpy as np
+import time
+
+class NewtonComputator:
+    def __init__(self, f, starting_point):
+        self.starting_point = starting_point
+        self.last_point = starting_point
+        self.points = [starting_point]
+        self.f = f
+        
+        self.gradient = nd.Gradient(f)
+        self.hessian = nd.Hessian(f)
+    
+    def start_computation(self, sleep_time=None):
+        self.should_compute = True 
+        self.compute(sleep_time)
+
+    def stop_computation(self):
+        self.should_compute = False
+    
+    def _get_newton_move(self, point):
+        return np.matmul(
+            np.linalg.inv(self.hessian(point)),-self.gradient(point)
+        )
+
+    def newton_iteration(self):
+        p0 = self.last_point
+        newton_move = self._get_newton_move(p0)
+        p = (p0[0] + newton_move[0], p0[1] + newton_move[1])
+        print(f'Current point: {p}, iteration: {self.i}')
+        self.points.append(p) 
+        self.last_point = p
+        self.i += 1
+
+    def compute(self, sleep_time = None):
+        self.i = 0
+        while self.should_compute:
+            if sleep_time: time.sleep(sleep_time)
+            self.newton_iteration()
+
+
+
---
+++ optimization_plotter.py
@@ -0,0 +1,2 @@
+def plot_computator(computator):
+    pass
